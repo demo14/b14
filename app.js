4d69f765085d0180b11b3aa9feba51edc65cc32f
NikshepAV <nikshep.av@gmail.com>
2018-02-23 15:23:28 +0530

    Org commit

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+// requiring packages
 var express		=	require('express'),
 	app 		=	express(),
 	mongoose 	=	require('mongoose');
 
+// requiring models
+var user		=	require('./models/user.js');
 
-mongoose.connect('mongodb://localhost/b14', {useMongoClient: true});
 
+// requiring routes
+var indexRoutes = 	require('./routes/index.js');
+
+
+
+// local mongoDB
+mongoose.connect('mongodb://localhost/b14');
+
+// custom requirements
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 
-app.lsiten(portNum = 3000, function() {
+// using routes
+app.use('/', indexRoutes);
+
+
+// local deployment
+app.listen(portNum = 3000, function() {
 	console.log('Server online at port ' + portNum);
 });
